992ef7a677f4cc0be0ae07d260b37b2dd942d0f3
Samkotian07 <151937595+Samkotian07@users.noreply.github.com>
2025-03-08 16:53:48 +0530

    devere

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Home from "./Pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" component={Home} />
-    </Routes>
-    </BrowserRouter>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+   </BrowserRouter>
   )
 }
 
